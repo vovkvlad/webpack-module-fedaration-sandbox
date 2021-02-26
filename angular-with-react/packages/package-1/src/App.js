f13f729dc7ea6d2791f4edfea8b5eb3ca716bd4f
Snizhana Mamchak <snizhana.mamchak@datarobot.com>
2021-02-26 12:08:28 +0200

    Create angular with react boilerplate

---
+++ angular-with-react/packages/package-1/src/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+export function App2() {
+  return(
+    <div className="app-2-contaier">
+      <h2>Inner content of package 1</h2>
+    </div>
+  )
+}
+
+export default App2;
---
+++ angular-with-react/packages/package-1/webpack.config.js
@@ -0,0 +1,62 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const packageJsonDeps = require('./package.json').dependencies;
+
+const { ModuleFederationPlugin } = webpack.container;
+
+const path = require('path');
+
+const SRC_DIR = path.resolve(__dirname, './src');
+const ENTRY_POINT = path.resolve(SRC_DIR, './index.js');
+const DIST_DIR = path.resolve(__dirname, './dist');
+const APP_TEMPLATE = path.resolve(__dirname, './index.html');
+
+module.exports = {
+  entry: ENTRY_POINT,
+  output: {
+    path: DIST_DIR,
+    filename: 'js/[name].bundle.[hash].js',
+  },
+  devServer: {
+    contentBase: DIST_DIR,
+    port: 2001,
+    historyApiFallback: true,
+    hot: true,
+    open: false,
+    clientLogLevel: 'debug',
+  },
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      }
+    ]
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "package1",
+      library: { type: "var", name: "package1" },
+      filename: "remoteEntry.js",
+      exposes: {
+        "./package1": "./src/App",
+      },
+      shared: {
+        ...packageJsonDeps,
+        react: { singleton: true, eager: true, requiredVersion: packageJsonDeps.react, },
+        "react-dom": { singleton: true, eager: true, requiredVersion: packageJsonDeps["react-dom"] }
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: APP_TEMPLATE,
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.ProgressPlugin({
+      activeModules: true,
+    }),
+  ],
+};
