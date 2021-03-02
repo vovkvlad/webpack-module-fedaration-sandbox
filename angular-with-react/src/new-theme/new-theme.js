d578fe793ded9fa77178f915fa2a376960f29a9b
Snizhana Mamchak <snizhana.mamchak@datarobot.com>
2021-03-02 11:52:09 +0200

    Create new theme page

---
+++ angular-with-react/src/new-theme/new-theme.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class NewTheme extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 style={{color: 'red'}}>Hello from react class component</h1>
+      </div>
+    );
+  }
+}
--- angular-with-react/src/routes.js
+++ angular-with-react/src/routes.js
@@ -1,21 +1,31 @@
 import app from "./app.module";
 import Home from "./home/home.component";
+import NewTheme from "./new-theme/new-theme";
+
+const states = [
+  {
+    name: "home",
+    url: "/",
+    template: "<home></home>",
+    resolve: {
+      component() {
+        Home.register();
+      },
+    },
+  },
+  {
+    name: "new",
+    url: "/new",
+    component: NewTheme,
+  },
+];
 
 app.config([
-  "$stateProvider",
   "$locationProvider",
-  function ($stateProvider, $locationProvider) {
-    $locationProvider.html5Mode({ enabled: true, requireBase: false });
-  
-    $stateProvider.state({
-      name: "home",
-      url: "/",
-      template: '<home></home>',
-      resolve: {
-        component() {
-          Home.register();
-        },
-      },
-    });
+  "$uiRouterProvider",
+  function ($locationProvider, $uiRouterProvider) {
+    $locationProvider.html5Mode({enabled: true, requireBase: false});
+
+    states.forEach((state) => $uiRouterProvider.stateRegistry.register(state));
   },
 ]);
