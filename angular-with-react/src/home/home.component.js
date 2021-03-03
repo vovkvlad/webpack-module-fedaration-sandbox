9cba475748a7fea817c049f1ea6b153c9c7512f1
Snizhana Mamchak <snizhana.mamchak@datarobot.com>
2021-03-03 15:13:20 +0200

    Preserve theme on page reload

--- angular-with-react/src/home/home.component.js
+++ angular-with-react/src/home/home.component.js
@@ -1,5 +1,6 @@
 import NgComponent from "../ng-component";
 import {$rootScope} from "../ng";
+import ColorService from "../data/color.service";
 import template from "./home.html";
 import "./home.css";
 
@@ -8,11 +9,23 @@ export default class Home extends NgComponent {
 
   static template = template;
 
+  constructor() {
+    super();
+
+    this.colorService = ColorService.getInstance();
+  }
+
   $onInit() {
     this.onInit();
   }
 
-  onInit() {
+  async onInit() {
+    await this.colorService.fetch();
+
+    this.setState({
+      themeColor: this.colorService.themeColor,
+    });
+
     $rootScope.$on('theme:changed', (event, data) => {
       this.setState({
         themeColor: data,
