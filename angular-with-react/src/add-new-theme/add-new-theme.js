057b4ebc9fd0f4efc6fa2aa9e317e1b3f41a4b32
Snizhana Mamchak <snizhana.mamchak@datarobot.com>
2021-03-03 15:00:15 +0200

    Add redirect on color submission

--- angular-with-react/src/add-new-theme/add-new-theme.js
+++ angular-with-react/src/add-new-theme/add-new-theme.js
@@ -2,19 +2,21 @@ import React from "react";
 
 import RemotePackage1 from "package1/package1";
 import ColorService from "../data/color.service";
+import {$state} from "../ng";
 
 export default class AddNewTheme extends React.Component {
   constructor(props) {
     super(props);
     this.colorService = ColorService.getInstance();
+    this.addColor = this.addColor.bind(this);
+  }
+
+  addColor(color) {
+    this.colorService.addColor(color);
+    $state.go('home.switcher');
   }
 
   render() {
-    return (
-      <RemotePackage1
-        onChange={this.colorService.addColor}
-        themeColor={this.colorService.themeColor}
-      />
-    );
+    return <RemotePackage1 onChange={this.addColor} />;
   }
 }
