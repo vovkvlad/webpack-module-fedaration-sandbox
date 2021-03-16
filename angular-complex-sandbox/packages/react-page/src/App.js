4ec47a5b30a50780a078494a3c71592968cac7d5
Vlad Vovk <vlad.vovk@datarobot.com>
2021-03-16 17:59:48 +0200

    fixed nested react router issues

--- angular-complex-sandbox/packages/react-page/src/App.js
+++ angular-complex-sandbox/packages/react-page/src/App.js
@@ -1,39 +1,42 @@
 import React from 'react';
 import { UIView, UISrefActive, UISref } from '@uirouter/react';
+import { UIRouterContextComponent } from '@uirouter/react-hybrid';
 
 import { UserPermissions } from './components/user-permissions/user-permissions';
 
 export default function App() {
   return (
-    <div className="react-page-container">
-      <h1>This is top level page that is rendered using react</h1>
-      <p>it also uses User permissions service to list all permissions below:</p>
+    <UIRouterContextComponent>
+      <div className="react-page-container">
+        <h1>This is top level page that is rendered using react</h1>
+        <p>it also uses User permissions service to list all permissions below:</p>
 
-      <UserPermissions/>
+        <UserPermissions/>
 
-      <hr/>
+        <hr/>
 
-      <p>If you want to add subrouting - just at the required stuff here. (or probably I'll do it later)</p>
-      <p>Okay, okay, I've done that too:</p>
+        <p>If you want to add subrouting - just at the required stuff here. (or probably I'll do it later)</p>
+        <p>Okay, okay, I've done that too:</p>
 
-      <ul className="react-tabs-container">
-        <li className="react-tab-item">
-          <UISrefActive class="react-tab-active">
-            <UISref to="app.react-page.sub-page-1">
-              <a>React Sub Page 1</a>
-            </UISref>
-          </UISrefActive>
-        </li>
-        <li className="react-tab-item">
-          <UISrefActive class="react-tab-active">
-            <UISref to="app.react-page.sub-page-2">
-              <a>React Sub Page 2</a>
-            </UISref>
-          </UISrefActive>
-        </li>
-      </ul>
+        <ul className="react-tabs-container">
+          <li className="react-tab-item">
+            <UISrefActive class="react-tab-active">
+              <UISref to="app.react-page.sub-page-1">
+                <a>React Sub Page 1</a>
+              </UISref>
+            </UISrefActive>
+          </li>
+          <li className="react-tab-item">
+            <UISrefActive class="react-tab-active">
+              <UISref to="app.react-page.sub-page-2">
+                <a>React Sub Page 2</a>
+              </UISref>
+            </UISrefActive>
+          </li>
+        </ul>
 
-      <UIView/>
-    </div>
+        <UIView/>
+      </div>
+    </UIRouterContextComponent>
   );
 }
